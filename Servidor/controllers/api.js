f98eaee23b0d71b83bccb125c605499a89596248
LuisEnVilla <villak@hotmail.es>
2014-12-07 12:03:40 -0600

    api

--- Servidor/controllers/api.js
+++ Servidor/controllers/api.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/ruta/:ruta', function(req, res) {
-	//db.coordis.
+	db.coordis.find({ruta : req.params.ruta },function (err, coordenadas){
+		if (err) res.render('error', {
+	    	message: err.message,
+	        error: {}});
+	    else {
+	    	res.send({coords : coordenadas});
+	    }
+
+	});
+});
+
+router.post('/ruta/:ruta', function(req, res) {
+	var coordenada = new db.coordis({
+		ruta: req.params.ruta,
+	    lat: req.body.telefono,
+	    lng: req.body.telefono,
+	    date:req.body.telefono
+		});
+		mensajes.save();
 });
 
 module.exports = router;
